15c60a7d3a3fd527577426614275f0335df8c55d
jarsbe <jarsbe@gmail.com>
2015-08-09 12:08:58 +0100

    Moved directories.

--- src/client.jsx
+++ src/client.jsx
@@ -1,18 +1,20 @@
-import 'babel/polyfill';
 import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import App from '../shared/components/app';
 import * as reducers from '../shared/reducers/index';
 
-const store = createStore(combineReducers(reducers));
+const combinedReducers = combineReducers(reducers);
+const store = createStore();
+
+function app() {
+  return <App />;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   React.render(
     <Provider store={store}>
-      {() =>
-        <App />
-      }
+      {app}
     </Provider>,
     document.getElementById('app')
   );
